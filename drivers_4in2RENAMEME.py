41fda8cc69cefd38685eac6048ae43aa3d873486
BTend24 <167252673+BTend24@users.noreply.github.com>
2024-05-19 13:02:22 -0300

    Update drivers_4in2RENAMEME.py

--- drivers_4in2RENAMEME.py
+++ drivers_4in2RENAMEME.py
@@ -1,25 +1,33 @@
-# Modified EPD4in2.py for V2
-
 from PIL import Image
 from papertty.drivers.drivers_base import GPIO
-from papertty.drivers.drivers_consts import EPD4in2const
+from papertty.drivers.drivers_consts import EPD4in2v2const
 from papertty.drivers.drivers_partial import WavesharePartial
 
-class EPD4in2(WavesharePartial, EPD4in2const):
-    """WaveShare 4.2" V2 """
+class EPD4in2v2(WavesharePartial, EPD4in2v2const):
+    """WaveShare 4.2" """
+
+    # code adapted from  epd_4in2.c
+    # https://github.com/waveshare/e-paper/blob/8973995e53cb78bac6d1f8a66c2d398c18392f71/raspberrypi%26jetsonnano/c/lib/e-paper/epd_4in2.c
 
     def __init__(self):
         super(WavesharePartial, self).__init__(name='4.2"',
                                                width=400,
                                                height=300)
         self.supports_partial = True
-        self.frame_buffer = [0x00] * (self.width * self.height // 8)
+        self.frame_buffer = [0xff] * (self.width * self.height // 8)
+        self.update_counter = 0  # Add a counter
+
+    def wait_until_idle(self):
+        while self.digital_read(self.BUSY_PIN) == 1:
+            self.delay_ms(20)
 
     def set_setting(self, command, data):
+        print(f"set_setting")
         self.send_command(command)
         self.send_data(data)
 
     def set_resolution(self):
+        print(f"set_resolution")
         self.set_setting(self.RESOLUTION_SETTING,
                          [(self.width >> 8) & 0xff,
                           self.width & 0xff,
@@ -27,12 +35,13 @@ class EPD4in2(WavesharePartial, EPD4in2const):
                           self.height & 0xff])
 
     def reset(self):
+        print(f"reset")
         self.digital_write(self.RST_PIN, GPIO.HIGH)
-        self.delay_ms(200)
+        self.delay_ms(100)
         self.digital_write(self.RST_PIN, GPIO.LOW)
-        self.delay_ms(200)
+        self.delay_ms(2)
         self.digital_write(self.RST_PIN, GPIO.HIGH)
-        self.delay_ms(200)
+        self.delay_ms(100)
 
     def send_command(self, command):
         self.digital_write(self.DC_PIN, GPIO.LOW)
@@ -43,98 +52,152 @@ class EPD4in2(WavesharePartial, EPD4in2const):
     def send_data(self, data):
         self.digital_write(self.DC_PIN, GPIO.HIGH)
         self.digital_write(self.CS_PIN, GPIO.LOW)
-        if isinstance(data, list):
-            self.spi_transfer(data)
+        if type(data) == list:
+            for d in data:
+                self.spi_transfer([d])
         else:
             self.spi_transfer([data])
         self.digital_write(self.CS_PIN, GPIO.HIGH)
 
-    def wait_until_idle(self):
-        self.send_command(self.GET_STATUS)
-        while self.digital_read(self.BUSY_PIN) == 0:
-            self.send_command(self.GET_STATUS)
-            self.delay_ms(100)
-
     def turn_on_display(self):
-        self.send_command(self.DISPLAY_REFRESH)
-        self.delay_ms(100)
+        print(f"turn_on_display")
+        self.send_command(0x22) #Display Update Control
+        self.send_data(0xF7)
+        self.send_command(0x20) #Activate Display Update Sequence
         self.wait_until_idle()
 
-    def full_set_lut(self):
-        self.set_setting(self.VCOM_LUT, self.LUT_VCOM0)
-        self.set_setting(self.W2W_LUT, self.LUT_WW)
-        self.set_setting(self.B2W_LUT, self.LUT_BW)
-        self.set_setting(self.W2B_LUT, self.LUT_WB)
-        self.set_setting(self.B2B_LUT, self.LUT_BB)
-
-    def partial_set_lut(self):
-        self.set_setting(self.VCOM_LUT, self.PARTIAL_LUT_VCOM1)
-        self.set_setting(self.W2W_LUT, self.PARTIAL_LUT_WW1)
-        self.set_setting(self.B2W_LUT, self.PARTIAL_LUT_BW1)
-        self.set_setting(self.W2B_LUT, self.PARTIAL_LUT_WB1)
-        self.set_setting(self.B2B_LUT, self.PARTIAL_LUT_BB1)
-
-    def gray_set_lut(self):
-        self.set_setting(self.VCOM_LUT, self.GRAY_LUT_VCOM)
-        self.set_setting(self.W2W_LUT, self.GRAY_LUT_WW)
-        self.set_setting(self.B2W_LUT, self.GRAY_LUT_BW)
-        self.set_setting(self.W2B_LUT, self.GRAY_LUT_WB)
-        self.set_setting(self.B2B_LUT, self.GRAY_LUT_BB)
-
-    def init_bw(self):
-        self.reset()
-        self.set_setting(self.POWER_SETTING, [0x03, 0x00, 0x2b, 0x2b, 0x13])
-        self.set_setting(self.BOOSTER_SOFT_START, [0x17, 0x17, 0x17])
-        self.send_command(self.POWER_ON)
+    def turn_on_display_fast(self):
+        print(f"turn_on_display_fast")
+        self.send_command(0x22) #Display Update Control
+        self.send_data(0xC7)
+        self.send_command(0x20) #Activate Display Update Sequence
         self.wait_until_idle()
-        self.set_setting(self.PANEL_SETTING, [0xbf, 0x0d])
-        self.set_setting(self.PLL_CONTROL, [0x3c])
-        self.set_resolution()
-        self.set_setting(self.VCM_DC_SETTING, [0x12])
-        self.set_setting(self.VCOM_AND_DATA_INTERVAL_SETTING, [0x97])
-        self.full_set_lut()
 
-    def init_gray(self):
-        self.reset()
-        self.set_setting(self.POWER_SETTING, [0x03, 0x00, 0x2b, 0x2b, 0x13])
-        self.set_setting(self.BOOSTER_SOFT_START, [0x17, 0x17, 0x17])
-        self.send_command(self.POWER_ON)
+    def turn_on_display_partial(self):
+        print(f"turn_on_display_partial")
+        self.send_command(0x22) #Display Update Control
+        self.send_data(0xFF)
+        self.send_command(0x20) #Activate Display Update Sequence
         self.wait_until_idle()
-        self.set_setting(self.PANEL_SETTING, [0x3f])
-        self.set_setting(self.PLL_CONTROL, [0x3c])
-        self.set_resolution()
-        self.set_setting(self.VCM_DC_SETTING, [0x12])
-        self.set_setting(self.VCOM_AND_DATA_INTERVAL_SETTING, [0x97])
+        
+    def turn_on_display_4gray(self):
+        print(f"turn_on_display_4gray")
+        self.send_command(0x22) #Display Update Control
+        self.send_data(0xCF)
+        self.send_command(0x20) #Activate Display Update Sequence
+        self.wait_until_idle()
 
-    def init(self, partial=True, gray=False):
+    def init(self, partial=True, gray=False, **kwargs):
+        print(f"init")
         self.partial_refresh = partial
         self.gray = gray
 
         if self.epd_init() != 0:
             return -1
 
-        if self.gray:
-            self.init_gray()
-        else:
-            self.init_bw()
+        self.reset()
+        self.wait_until_idle()
+
+        self.send_command(0x12) #SWRESET
+        self.wait_until_idle()
+
+        self.send_command(0x21)  # Display update control
+        self.send_data(0x40)
+        self.send_data(0x00)
+
+        self.send_command(0x3C)  # BorderWavefrom
+        self.send_data(0x05)
+
+        self.send_command(0x11)  # data entry mode
+        self.send_data(0x03)  # X-mode
+
+        self.send_command(0x44) 
+        self.send_data(0x00)
+        self.send_data(0x31)  
+        
+        self.send_command(0x45) 
+        self.send_data(0x00)
+        self.send_data(0x00)  
+        self.send_data(0x2B)
+        self.send_data(0x01)
+
+        self.send_command(0x4E) 
+        self.send_data(0x00)
+
+        self.send_command(0x4F) 
+        self.send_data(0x00)
+        self.send_data(0x00)  
+        self.wait_until_idle()
+
+        self.clear()
+
+    def init_fast(self, partial=True, gray=False, **kwargs):
+        print(f"init_fast")
+        self.partial_refresh = partial
+        self.gray = gray
+
+        if self.epd_init() != 0:
+            return -1
+
+        self.reset()
+        self.wait_until_idle()
+
+        self.send_command(0x12) #SWRESET
+        self.wait_until_idle()
+
+        self.send_command(0x21)  # Display update control
+        self.send_data(0x40)
+        self.send_data(0x00)
+
+        self.send_command(0x3C)  # BorderWavefrom
+        self.send_data(0x05)
+
+        self.send_command(0x1A)
+        self.send_data(0x5A)  
+
+        self.send_command(0x22)  # Load temperature value
+        self.send_data(0x91)  
+        self.send_command(0x20)  
+        self.wait_until_idle()
+
+        self.send_command(0x11)  # data  entry  mode
+        self.send_data(0x03)  # X-mode
+
+        self.send_command(0x44) 
+        self.send_data(0x00)
+        self.send_data(0x31)  
+        
+        self.send_command(0x45) 
+        self.send_data(0x00)
+        self.send_data(0x00)  
+        self.send_data(0x2B)
+        self.send_data(0x01)
+
+        self.send_command(0x4E) 
+        self.send_data(0x00)
+
+        self.send_command(0x4F) 
+        self.send_data(0x00)
+        self.send_data(0x00)  
+        self.wait_until_idle()
 
     def clear(self):
-        width = int(self.width // 8)
-        height = int(self.height)
+        print(f"clear")
+        if self.width % 8 == 0:
+            linewidth = int(self.width / 8)
+        else:
+            linewidth = int(self.width / 8) + 1
 
-        self.send_command(self.DATA_START_TRANSMISSION_1)
-        for i in range(width * height):
-            self.send_data(0xff)
+        self.send_command(0x24)
+        self.send_data([0xff] * int(self.height * linewidth))
 
-        self.send_command(self.DATA_START_TRANSMISSION_2)
-        for i in range(width * height):
-            self.send_data(0xff)
+        self.send_command(0x26)
+        self.send_data([0xff] * int(self.height * linewidth))
 
-        self.send_command(self.DISPLAY_REFRESH)
-        self.delay_ms(10)
         self.turn_on_display()
 
     def fill(self, color, fillsize):
+        print(f"fill: color {color}, fillsize {fillsize}")
         div, rem = divmod(self.height, fillsize)
         image = Image.new('1', (self.width, fillsize), color)
 
@@ -146,59 +209,57 @@ class EPD4in2(WavesharePartial, EPD4in2const):
             self.draw(0, div * fillsize, image)
 
     def display_full(self):
-        width = int(self.width // 8)
-        height = int(self.height)
+        print(f"display_full")
+        self.send_command(0x24)
+        self.send_data(self.frame_buffer)
 
-        self.send_command(self.DATA_START_TRANSMISSION_2)
-        for i in range(height):
-            for j in range(width):
-                self.send_data(self.frame_buffer[i * width + j])
+        self.send_command(0x26)
+        self.send_data(self.frame_buffer)
 
         self.turn_on_display()
 
-    def display_partial(self, x_start, y_start, x_end, y_end):
-        width = self.width // 8
+    def display_partial(self):
+        print(f"display partial")
+ 
+        self.send_command(0x3C)  # BorderWavefrom
+        self.send_data(0x80)
 
-        x_start = int(x_start if x_start % 8 == 0 else x_start // 8 * 8 + 8)
-        x_end = int(x_end if x_end % 8 == 0 else x_end // 8 * 8 + 8)
+        self.send_command(0x21)  # Display update control
+        self.send_data(0x00)
+        self.send_data(0x00)
 
-        self.set_setting(self.VCOM_AND_DATA_INTERVAL_SETTING, [0xf7])
-        self.delay_ms(100)
+        self.send_command(0x3C)  # BorderWavefrom
+        self.send_data(0x80)
 
-        self.set_setting(self.VCM_DC_SETTING, [0x08])
-        self.set_setting(self.VCOM_AND_DATA_INTERVAL_SETTING, [0x47])
-        self.partial_set_lut()
+        self.send_command(0x44) 
+        self.send_data(0x00)
+        self.send_data(0x31)  
+        
+        self.send_command(0x45) 
+        self.send_data(0x00)
+        self.send_data(0x00)  
+        self.send_data(0x2B)
+        self.send_data(0x01)
 
-        self.send_command(self.PARTIAL_IN)
-        self.set_setting(self.PARTIAL_WINDOW,
-                         [x_start     // 256, x_start     % 256,
-                          (x_end - 1) // 256, (x_end - 1) % 256,
-                          y_start     // 256, y_start     % 256,
-                          (y_end - 1) // 256, (y_end - 1) % 256,
-                          0x28])
+        self.send_command(0x4E) 
+        self.send_data(0x00)
 
-        self.send_command(self.DATA_START_TRANSMISSION_1)
-        for j in range(y_end - y_start):
-            idx = (y_start + j) * width + x_start // 8
-            for i in range((x_end - x_start) // 8):
-                self.send_data(self.frame_buffer[idx + i])
+        self.send_command(0x4F) 
+        self.send_data(0x00)
+        self.send_data(0x00) 
 
-        self.send_command(self.DATA_START_TRANSMISSION_2)
-        for j in range(y_end - y_start):
-            idx = (y_start + j) * width + x_start // 8
-            for i in range((x_end - x_start) // 8):
-                self.send_data(~self.frame_buffer[idx + i])
+        self.send_command(0x24) # WRITE_RAM
 
-        self.send_command(self.DISPLAY_REFRESH)
-        self.delay_ms(10)
-        self.turn_on_display()
+        self.send_data(self.frame_buffer)  
+        self.turn_on_display_partial()
 
     def sleep(self):
-        self.send_command(self.POWER_OFF)
-        self.wait_until_idle()
-        self.set_setting(self.DEEP_SLEEP, [0xa5])
+        print(f"sleep")
+        self.send_command(0x10)  # DEEP_SLEEP
+        self.send_data(0x01)
 
     def frame_buffer_to_image(self):
+        print(f"frame_buffer_to_image")
         im = Image.new('1', (self.width, self.height), "white")
         pi = im.load()
 
@@ -213,9 +274,12 @@ class EPD4in2(WavesharePartial, EPD4in2const):
         return im
 
     def set_frame_buffer(self, x, y, image):
+        print(f"set_frame_buffer, {x}, {y}, {image}")
         image_monocolor = image.convert('1')
         imwidth, imheight = image_monocolor.size
         pixels = image_monocolor.load()
+
+        print(f"running loop on image of size {imwidth}x{imheight}")
 
         for j in range(imheight):
             idxj = (y + j) * self.width // 8
@@ -223,15 +287,22 @@ class EPD4in2(WavesharePartial, EPD4in2const):
                 idiv, irem = divmod(x + i, 8)
                 mask = 0b10000000 >> irem
                 idxi = idiv
+
                 if pixels[i, j] != 0:
                     self.frame_buffer[idxi + idxj] |= mask
                 else:
                     self.frame_buffer[idxi + idxj] &= ~mask
 
     def draw(self, x, y, image):
+        print(f"draw, x:{x}, y:{y}, image:{image}")
+        self.set_frame_buffer(x, y, image)
+
         if self.partial_refresh:
-            self.set_frame_buffer(x, y, image)
-            self.display_partial(x, y, x + image.width, y + image.height)
+            self.display_partial()
         else:
-            self.set_frame_buffer(0, 0, image)
             self.display_full()
+        
+        self.update_counter += 1
+        if self.update_counter > 10:
+            print("Update counter exceeded, breaking loop")
+            return
